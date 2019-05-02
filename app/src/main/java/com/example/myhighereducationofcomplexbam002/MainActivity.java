2ea0d9f970c6cb61c1fecb7c22bcf15987fcc0c7
rohanimorteza <rohani.morteza@gmail.com>
2019-05-02 11:26:55 +0430

    013 980212 part-7 Copy DB test DB with TOAST

--- app/src/main/java/com/example/myhighereducationofcomplexbam002/MainActivity.java
+++ app/src/main/java/com/example/myhighereducationofcomplexbam002/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        dbHandler dbh = new dbHandler(this);
+        dbh.open();
+        Toast.makeText(getApplicationContext(),dbh.get_Name(4),Toast.LENGTH_LONG).show();
+        dbh.close();
+
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
