703586261cbe33592ef80b59e370eb0df94bb6db
rohanimorteza <rohani.morteza@gmail.com>
2019-05-02 10:14:40 +0430

    006 980212 part-2 Copy DB step-2

--- app/src/main/java/com/example/myhighereducationofcomplexbam002/dbHandler.java
+++ app/src/main/java/com/example/myhighereducationofcomplexbam002/dbHandler.java
@@ -1,4 +1,24 @@
 package com.example.myhighereducationofcomplexbam002;
 
-public class dbHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class dbHandler extends SQLiteOpenHelper {
+
+    
+
+    public dbHandler(Context context) {
+        super(context, "BAM", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
